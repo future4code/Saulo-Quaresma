8acfd424ed61c7f7c7431393fb102c7ae2e1120f
Settlers <ssaquo@gmail.com>
2019-11-24 21:29:03 -0300

    Semana4-aula4 Exercício 1 agora completo.

--- semana4/aula4-funcoes-de-array/controlador-de-despesas/controlador.js
+++ semana4/aula4-funcoes-de-array/controlador-de-despesas/controlador.js
@@ -1,52 +1,81 @@
+/**CRIA A CLASSE DESPESA */
 class despesa {
-   constructor(nome, valor, despesa, descricao){
-      this.nomeDesp = nome
+   constructor(valor, despesa, descricao){
       this.valores = valor
       this.tipoDesp = despesa
       this.descDesp = descricao
    }
 }
 
-const primeiroArray = []
+//CRIA UM ARRAY VAZIO
+const arrayDespesas = []
 
-function aoClicarNoBotaoCadastro() {
-   this.nomeDesp = document.getElementById("nomeDespesa").value,
-   this.valores = document.getElementById("valorDinheiro").value;
-   this.tipoDesp = document.getElementById("tipoDespesa").value;
-   this.descDesp = document.getElementById("descricao").value;
+//FUNÇÃO DE CADASTRAR OS DADOS NA PÁGINA HTML
+function aoClicarNoBotaoCadastro(){
+   if (document.getElementById("valorGasto").value == "" || document.getElementById("tipoDespesa") == "" || document.getElementById("descricao").value == "") {
+      alert("Por favor, preencha todos os campos!");           //alerta de campos vazios
+   }  else {
+      const valorDespesa = document.getElementById("valorGasto").value;
+      const tipoDespesa = document.getElementById("tipoDespesa").value;
+      const descDespesa = document.getElementById("descricao").value;
 
-   if(this.nomeDesp === "" || this.valores === "" || this.tipoDesp === "" || this.descDesp === "") {
-      alert("Por favor, preencha todos os campos!")
-
-   }  else if(this.nomeDesp !== "" || this.valores !== "" || this.tipoDesp !== "" || this.descDesp !== ""){
-      const novaDespesa = new despesa(nomeDesp, valores, tipoDesp, descDesp)
-      //console.log(novaDespesa)
-
-      primeiroArray.push(novaDespesa)
-      //console.log(primeiroArray)
-      listarNaPagina()
+      const novaDespesa = new despesa(valorDespesa, tipoDespesa, descDespesa)       //coleta informções para o novo array
+      arrayDespesas.push(novaDespesa)                                               //adicona em tudo em um novo array
+      //console.log(arrayDespesas)
+      const listaDeDespesas = document.getElementById("lista-despesa")
+      listaDeDespesas.innerHTML = ""
+      for(const elementos of arrayDespesas) {               //adiciona na página o que foi cadastrado
+         listaDeDespesas.innerHTML += "<div>" + "Valor: " + "<p>" + elementos.valores + "</p>" + "</div>";
+         listaDeDespesas.innerHTML += "<div>" + "Tipo de Despesa: " + "<p>" + elementos.tipoDesp + "</p>" + "</div>";
+         listaDeDespesas.innerHTML += "<div>" + "Descrição: " + "<p>" + elementos.descDesp + "</p>" + "</div>";
       }
-   document.getElementById("nomeDespesa").value = ""
-   document.getElementById("valorDinheiro").value = ""
-   document.getElementById("tipoDespesa").value = "casa"
-   document.getElementById("descricao").value = ""
-}
+      document.getElementById("valorGasto").value = "";                           //apaga o que foi digitado nos campos
+      document.getElementById("tipoDespesa").value = "Casa";
+      document.getElementById("descricao").value = "";
 
-function listarNaPagina(){
-   const listagem = document.getElementById("container-listaDespesa")
-   for(const index of primeiroArray){
-      listagem.innerHTML += "<div>" + "<p>" + "Nome da Despesa: " + index.nomeDesp + "</p>" + "</div>"
-      listagem.innerHTML += "<div>" + "<p>" + "Valor: " + "R$: " + index.valores + "</p>" + "</div>"
-      listagem.innerHTML += "<div>" + "<p>" + "Tipo de Despesa: " + index.tipoDesp + "</p>" + "</div>"
-      listagem.innerHTML += "<div>" + "<p>" +"Descrição: " + index.descDesp + "</p>" + "</div>"
+      somarValoresDespesas()
    }
 }
 
-function aoClicarBotaoFiltro(){
-
+//FUNÇÃO DO BOTÃO FILTRAR
+function aoClicarBotaoFiltro() {
+   if(document.getElementById("tipoDespesaDetalhes").value == "" || document.getElementById("minimo") == "" || document.getElementById("maximo").value == "") {
+      alert("Por favor, preencha todos os campos!");                  //alerta de campos vazios
+   } else {
+      const resultadosFiltro = document.getElementById("resultados-filtragem")
+      resultadosFiltro.innerHTML = ""
+      const tipoDespesa = document.getElementById("tipoDespesaDetalhes").value;
+      //console.log(tipoDespesa)
+      const valorMinimo = Number(document.getElementById("minimo").value)
+      //console.log(valorMinimo)
+      const valorMaximo = Number(document.getElementById("maximo").value)   
+      //console.log(valorMaximo)   
+      const despesasFiltradas = arrayDespesas.filter( despesa => {                  //filtra o que foi desejado nos filtros o que foi cadastrado
+         return despesa.tipoDesp === tipoDespesa && despesa.valores >= valorMinimo && despesa.valores <= valorMaximo
+      })
+      for(const elementos of despesasFiltradas) {                       //adiciona na página o que foi cadastrado
+         resultadosFiltro.innerHTML += "<div>" + "Valor: " + "<p>" + elementos.valores + "</p>" + "</div>";
+         resultadosFiltro.innerHTML += "<div>" + "Tipo de Despesa: " + "<p>" + elementos.tipoDesp + "</p>" + "</div>";
+         resultadosFiltro.innerHTML += "<div>" + "Descrição: " + "<p>" + elementos.descDesp + "</p>" + "</div>";
+      }
+      document.getElementById("tipoDespesa").value = "Casa";            //apaga o que foi digitado nos campos
+      document.getElementById("minimo").value = "";
+      document.getElementById("maximo").value = "";
+   }
 }
 
-function aoClicarBotaoLimparFiltro(){
-   
+//FUNÇÃO LIMPAR FILTROS
+function aoClicarBotãoLimparFiltros(){
+   const resultadosFiltro = document.getElementById('resultados-filtragem')
+   resultadosFiltro.innerHTML = ""                    //apaga o que foi filtrado
+   alert("Os resultados foram limpos!")               //amostra que os resultados foram limpados da página
 }
 
+//FUNÇÃO DO EXTRATO DE VALORES
+function somarValoresDespesas() {
+   const insereExtrato = document.getElementById("valorTotalExtrato")
+   let valorTotalExtrato = arrayDespesas.reduce((valorTotal, quantValor) =>
+      parseInt(valorTotal) + parseInt(quantValor.valores), 0)           //soma os valores adicionados no primeiro index do array
+
+   insereExtrato.innerHTML = "Valor Total: " + "<p>" + " R$ " + valorTotalExtrato + "</p>";           //adiciona na página o total das despesas
+}
