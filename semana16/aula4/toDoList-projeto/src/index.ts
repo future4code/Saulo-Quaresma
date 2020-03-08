76a25fe735ccf6773764819f9808fcb71e555601
Settlers <ssaquo@gmail.com>
2020-03-07 21:19:01 -0300

    subindo resto do exercício feito

--- semana16/aula4/toDoList-projeto/src/index.ts
+++ semana16/aula4/toDoList-projeto/src/index.ts
@@ -20,20 +20,28 @@ app.get('/', (req: Request, res: Response) => {
    const resposta = {
       endpoints: {
          '/': "página inicial",
-         '/createUser': "cria o usuário com todas as informações necessárias através do postman",
-         '/editUser/:userId': "edita o nickname do usuário pelo postman através do id",
+         '/createUser': "cria um usuário com todas as informações necessárias através do Postman",
+         '/editUser/:userId': "edita o nickname do usuário pelo Postman através do id",
          '/deleteUser/:userId': "deleta um usuário passando um id",
          '/getUserByIdOrName/:data': 'pega um usuário pelo nome ou id',
          '/getAllUsers/?order&birthday=': "pegar todos os usuários, mostrando somente o nickname. Podendo ser ordem alfabética ou data de nascimento",
+         '/createTask': "cria uma tarefa com todas as informações necessárias através do Postman",
+         '/editTask/:userId': "edita a descrição ou tempo limite de uma tarefa passando o id dela pelo Postman",
+         '/addTaskResponsibleUser/:taskId/:userId': "Atribui um id de um usuário à uma tarefa pelo Postman",
+         '/getTasksCreatedByUser/:userId': "pega as tarefas criada por um usuário pelo id da tarefa",
+         '/getTasksGivenToUser/:userId': "pega as tarefas dadas à um usuário pelo id da tarefa",
+         '/getAllTasks': "pega todas as tarefas e ordenar por descrição ou nome do usuários alfabeticamente"
       }
    }
    res.send(resposta)
 });
 
-//Criar novos usuários
+//PARTE DE USUÁRIOS
+
+//Criar novos usuários pelo postman
 app.post('/createUser', (req: Request, res: Response) => {
    const nonQuery = connection.insert(req.body).into('usuarios');
-   nonQuery.then((result) => {
+   nonQuery.then(() => {
       res.send(`O usuário ${req.body.name} foi criado com sucesso!`);
    }).catch((err) => {
       res.send(err);
@@ -43,7 +51,7 @@ app.post('/createUser', (req: Request, res: Response) => {
 //Editar nickname de um usuário pelo id
 app.put('/editUser/:userId', (req: Request, res: Response) => {
    const nonQuery = connection('usuarios').where({ 'id': req.params.userId }).update({ nickname: req.body.nickname });
-   nonQuery.then((result) => {
+   nonQuery.then(() => {
       res.send(`O nickname do usuário foi editado com sucesso!`)
    }).catch((err) => {
       res.send(err)
@@ -82,6 +90,106 @@ app.get('/getAllUsers/', (req: Request, res: Response) => {
 
    if (birthday) {
       query = query.where('birthday', birthday)
+   };
+
+   query.then((result) => {
+      res.send(result)
+   }).catch((err) => {
+      res.send(err)
+   });
+});
+
+//PARTE DE TAREFAS
+
+//Criar uma nova tarefa pelo postman
+app.post('/createTask', (req: Request, res: Response) => {
+   const nonQuery = connection.insert(req.body).into('tarefas');
+   nonQuery.then(() => {
+      res.send(`A tarefa ${req.body.description} foi criada com sucesso!`)
+   }).catch((err) => {
+      res.send(err);
+   });
+});
+
+//Editar a descrição ou tempo limite da tarefa passando o id dela pelo postman
+app.put('/editTask/:userId', (req: Request, res: Response) => {
+   const description = req.body.description;
+   const timeLimit = req.body.time_limit;
+   let nonQuery = connection('tarefas').where({'id': req.params.userId});
+      
+   if(description) {
+      nonQuery.update({description: req.body.description})
+   };
+
+   if(timeLimit) {
+      nonQuery.update({time_limit: req.body.time_limit})
+   }
+   nonQuery.then(() => {
+      res.send(`Descrição atualizada com sucesso!`)
+   }).catch((err) => {
+      res.send(err)
+   });
+});
+
+//Atribuir um id de um usuário à uma tarefa pelo postman
+app.put('/addTaskResponsibleUser/:taskId/:userId', (req: Request, res: Response) => {
+   const nonQuery = connection('tarefas').where({'id': req.params.taskId}).update({responsible_userId: req.params.userId});
+   nonQuery.then(() => {
+      res.send('O id do usuário responsável foi atribuído à tarefa com sucesso!')
+   }).catch((err) => {
+      res.send(err)
+   });
+});
+
+//Pegar tarefas criada por um usuário pelo id da tarefa
+app.get('/getTasksCreatedByUser/:userId', (req: Request, res: Response) => {
+   const limitTime = req.query.timeLimit;
+   let query = connection.select('*').from('tarefas').where('requesting_userId', req.params.userId);
+   
+   if(limitTime) {
+      query = query.andWhere('time_limit', limitTime)
+   }
+
+   query.then((result) => {
+      res.send(result)
+   }).catch((err) => {
+      res.send(err)
+   })
+});
+
+//Pegar tarefas dadas à um usuário pelo id da tarefa
+app.get('/getTasksGivenToUser/:userId', (req: Request, res: Response) => {
+   const limitTime = req.query.timeLimit;
+   let query = connection.select('*').from('tarefas').where('responsible_userId', req.params.userId);
+   
+   if(limitTime) {
+      query = query.andWhere('time_limit', limitTime)
+   }
+
+   query.then((result) => {
+      res.send(result)
+   }).catch((err) => {
+      res.send(err)
+   });
+});
+
+//Pegar todas as tarefas e ordenar por descrição ou nome do usuários alfabeticamente
+app.get('/getAllTasks', (req: Request, res: Response) => {
+   const orderByDescription = req.query.orderByDescription;
+   const orderByName = req.query.orderByName;
+   const timeLimit = req.query.timeLimit;
+   let query = connection.select('*').from('tarefas').join('usuarios', 'tarefas.requesting_UserId', '=', 'usuarios.id');
+
+   if(orderByDescription){
+      query = query.orderBy('description', orderByDescription)
+   };
+
+   if(orderByName) {
+      query = query.orderBy('name', orderByName)
+   };
+
+   if(timeLimit) {
+      query = query.where('time_limit', timeLimit)
    };
 
    query.then((result) => {
