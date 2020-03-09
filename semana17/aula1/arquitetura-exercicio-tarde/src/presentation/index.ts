277e283a94fe2b097fd606a927808b14321657b2
Settlers <ssaquo@gmail.com>
2020-03-09 17:05:13 -0300

    exercício feito até o 2

---
+++ semana17/aula1/arquitetura-exercicio-tarde/src/presentation/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import { signupEndpoint } from "./endpoints/signup";
+import { getUserByEmail } from "./endpoints/getUserByEmail";
+import { getAllUsers } from "./endpoints/getAllUsers";
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", signupEndpoint);
+
+app.get("/user", getUserByEmail);
+
+app.get("/user/all", getAllUsers);
+
+app.put("/editUser/:id", );
+
+export default app;
