7364172752eef0626347481cfd8a50fa211e882d
Settlers <ssaquo@gmail.com>
2020-04-08 00:02:18 -0300

    Decidido o estilo do site, home page estático completado

--- semana21/futuretube/frontend/src/containers/HomePage/index.js
+++ semana21/futuretube/frontend/src/containers/HomePage/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer';
+import VideoCards from '../../components/VideoCards/index';
 
 class HomePage extends Component {
    render() {
       return (
          <main className="main">
             <Header />
+
+            <VideoCards/>
+
             <Footer />
          </main>
       )
